f38be2aeaa2207f70bf07d68906962e781a1ce8c
Yousef Ashraf <bosayousef981@gmail.com>
2022-09-27 23:09:09 +0200

    Finalized product

--- src/services/product/getProducts.js
+++ src/services/product/getProducts.js
@@ -1,18 +1,19 @@
-import Product from "../../helpers/db/product.db.js";
-import Cat from "../../helpers/db/cat.db.js";
-import { okResponse } from "./../../helpers/functions/ResponseHandler.js";
+import Product from '../../helpers/db/product.db.js';
+import Cat from '../../helpers/db/cat.db.js';
+import { okResponse } from './../../helpers/functions/ResponseHandler.js';
 export function getProducts(req, res, next) {
-  try {
-    let category = Cat.forEach((cat) => {
-      cat.name;
-    });
-    let product = Product.forEach((product) => {
-      product.name, product.quantity;
-    });
-    let arr = [];
-    console.log(product, category);
-    return okResponse(res, "Product fetched succesfully", product);
-  } catch (err) {
-    next(err);
-  }
+	try {
+		const products = Product.map((p) => {
+			const category = Cat.find((c) => c.id == p.catId);
+			const product = {
+				...p,
+				category,
+			};
+			delete product.catId;
+			return product;
+		});
+		return okResponse(res, 'Product fetched succesfully', products);
+	} catch (err) {
+		next(err);
+	}
 }

a4f3f567cd2b715ba56991633d287c9457a2f804
mohamed tarek <mt1348@fayoum.edu.eg>
2022-09-27 21:29:55 +0200

    first commit

---
+++ src/helpers/db/users.db.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    id: 1,
+    name: "John Adel",
+    phoneNumber: "23",
+    email: "",
+  },
+];
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import UserRouter from "./controllers/users.controller.js";
+import logger from "./helpers/middlewares/logger.js";
+import dotenv from "dotenv";
+import errorHandler from "./helpers/middlewares/errorHandler.js";
+dotenv.config();
+const app = express();
+
+// -- Middleware --
+app.use(express.json());
+app.use(logger);
+
+// -- Routes --
+app.use("/users", UserRouter);
+
+app.use(errorHandler);
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("Server is running on port 3000");
+  console.log(`http://localhost:${PORT}`);
+});
+
+export default app;
